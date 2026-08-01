c410fce0241cca9620a207685e292cb88aa23ff1
hussainali2023 <iamhussainali2@gmail.com>
2026-08-01 19:45:45 +0530

    configure app and server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
-import express, { type Application } from "express"
+import express, { type Application, type Request, type Response } from "express"
+import cors from "cors"
+import config from "./config"
+import cookieParser from "cookie-parser"
+import { notFound } from "./middlewares/notFound"
+import { globalErrorHandler } from "./middlewares/globalErrorHandler"
 
 const app: Application = express()
 
+app.use(cors(
+    {origin:config.app_url,
+    credentials:true
+    }
+))
 
 
-app.get("/", (req, res)=>{
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req:Request, res:Response)=>{
     res.send("Server is running properly")
 })
+
+app.use(notFound)
+app.use(globalErrorHandler)
 
 export default app;
